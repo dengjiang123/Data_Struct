22a6617ce8d10f8b739cdace85178b8fc5ab9fed
dengjiang123 <47911802+dengjiang123@users.noreply.github.com>
2021-03-15 22:37:51 +0800

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -22,3 +22,11 @@ void convert(Stack<char>& S, __int64 n, int base) {
 		convert(S, n / base, base);
 	}
 }
+
+void convertRecursive(Stack<char>& S, __int64 n, int base) {
+	static char digit[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	while (n) {
+		S.push(digit[n % base]);
+		n /= base;
+	}
+}
